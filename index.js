d146671a62d3fc41533dc3763a8f961b86225427
dkifashion <dkiwomanfashion@gmail.com>
2023-05-15 15:51:58 +0700

    config body-parser, use middleware and require routers

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var express = require('express');
 
 var app = express();
 
+// Body-parser configuration
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
+require('./routers/getList.router')(app);
+const _AuthMiddleWare = require('./common/_AuthMiddleWare');
+app.use(_AuthMiddleWare.isAuth);
+require('./routers/category.router')(app);
+require('./routers/topic.router')(app);
+require('./routers/audio.router')(app);
+
 app.listen(3000, function () {
     console.log("Server is running at http://localhost:3000/");  
 });
