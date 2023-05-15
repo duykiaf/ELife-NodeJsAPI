3f1749d8be06997b640c5132e6f1aae776da6397
dkifashion <dkiwomanfashion@gmail.com>
2023-05-15 15:46:23 +0700

    handle category controller

---
+++ controllers/category.controller.js
@@ -0,0 +1,27 @@
+var categoryModel = require('../models/category.model');
+
+exports.getCategoriesList = function (req, res) {
+    categoryModel.getCategoriesList(function (data) {
+        res.send({ categories: data });
+    })
+}
+
+exports.getById = function (req, res) {
+    categoryModel.getById(req.params.id, function (data) {
+        res.send({ category: data });
+    });
+}
+
+exports.addCategory = function (req, res) {
+    var data = req.body;
+    categoryModel.addCategory(data, function (response) {
+        res.send({ category: response });
+    });
+}
+
+exports.updateCategory = function (req, res) {
+    var data = req.body;
+    categoryModel.updateCategory(data, function (response) {
+        res.send({ category: response });
+    });
+}
