a097c3cb3f76a895818d86c1e1053101fd3e5781
dkifashion <dkiwomanfashion@gmail.com>
2023-05-16 15:53:58 +0700

    replace database information hardcode

--- common/connection.js
+++ common/connection.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
+require('dotenv').config();
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '27092002',
-    database: 'elife'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect(function(err) {
