e607c3d74436115a516a5732a051ac303cdba1a5
dkifashion <dkiwomanfashion@gmail.com>
2023-06-03 10:52:29 +0700

    update isAuth: check exists refreshTokenFromClient

--- common/_AuthMiddleWare.js
+++ common/_AuthMiddleWare.js
@@ -3,30 +3,30 @@ const _JWT = require('./_JWT');
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || "access-token-secret-default-dnvisme-meisdnv-@";
 
 let isAuth = async (req, res, next) => {
-    const tokenFromClient = req.body.token || req.query.token || req.headers["x-access-token"];
-    if (tokenFromClient) {
-        // Nếu tồn tại token
-        try {
-            // Thực hiện giải mã token xem có hợp lệ hay không?
-            const decoded = await _JWT.verifyToken(tokenFromClient, accessTokenSecret);
-            // Nếu token hợp lệ, lưu thông tin giải mã được vào đối tượng req, dùng cho các xử lý ở phía sau.
+    const accessTokenFromClient = req.header('x-access-token');
+    const refreshTokenFromClient = req.header('x-refresh-token');
+    if (accessTokenFromClient && refreshTokenFromClient) {
+        _JWT.verifyToken(accessTokenFromClient, accessTokenSecret, (decoded) => {
+            // success
+            console.log("isAuth: verify token successfully");
             req.jwtDecoded = decoded;
-            // Cho phép req đi tiếp sang controller.
             next();
-        } catch (error) {
-            // Nếu giải mã gặp lỗi: Không đúng, hết hạn...etc:
-            console.log("Error while verify token:", error);
-            return res.status(401).json({
-                message: 'Unauthorized.',
-            });
-        }
-    } else {
-        // Không tìm thấy token trong request
-        return res.status(403).send({
-            message: 'No token provided.',
+        }, (error) => {
+            console.log(error.message);
+            console.log("isAuth: unauthorized");
+            res.status(401).send(JSON.stringify({
+                message: "Unauthorized"
+            }));
         });
+    } else {
+        console.log("isAuth: token not found");
+        res.status(400).send(JSON.stringify({
+            message: "Token not found"
+        }));
     }
+
 }
+
 module.exports = {
     isAuth: isAuth
 };
