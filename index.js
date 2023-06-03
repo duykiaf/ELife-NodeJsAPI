ed71d4417b428a58c53900d8207c5e7ef2cdbdf8
dkifashion <dkiwomanfashion@gmail.com>
2023-06-03 10:49:39 +0700

    format code

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express');
-
 var app = express();
 
 // Body-parser configuration
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 require('./routers/auth.router')(app);
@@ -16,5 +15,5 @@ require('./routers/topic.router')(app);
 require('./routers/audio.router')(app);
 
 app.listen(3000, function () {
-    console.log("Server is running at http://localhost:3000/");  
+    console.log("Server is running at http://localhost:3000/");
 });
